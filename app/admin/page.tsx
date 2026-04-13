5e5e7db4079e1dd5755ee82a6ef9072f4756e101
IvanaVelickovic <iv56160@fer.hr>
2026-04-13 18:23:35 +0200

    add redirection from login page to dashboard if the user is already logged in

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import AdminLogin from "@/components/AdminLogin";
+import { useRequireAuth } from "@/lib/useRequireAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Admin() {
+  const router = useRouter();
+  const { user, loading } = useRequireAuth();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/admin/dashboard");
+    }
+  }, [user]);
+
+  if (loading) return null;
+  if (user) return null;
+
   return (
     <div className="h-full">
       <AdminLogin></AdminLogin>
